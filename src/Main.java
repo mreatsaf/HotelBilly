71439cb484879117ac9de0e239af08b63b7baf6e
jeiyanet <jeiya5934@proton.me>
2024-11-20 05:20:12 +0800

    added welcome, book or exit, input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner input = new Scanner(System.in);
+
+        welcome();
+        System.out.println("Would you like to Book a night or Exit?");
+        System.out.println("[1] Book a night");
+        System.out.println("[0] Book a night");
+        System.out.print("~> ");
+        int userChoice = input.nextInt();
+
+        switch (userChoice) {
+            case 1:
+                
+                break;
+        
+            case 0:
+                System.exit(0);
+
+            default:
+                break;
+        }
+    }
+
+    private static void welcome() {
+        System.out.println("WELCOME TO HOTEL DE LUNA");
     }
 }
